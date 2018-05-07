162c030cbf6df402c1ccb0a43c8573cc89ba196c
renansaatman <renansaatman@hotmail.com>
2018-05-07 10:46:51 -0300

    mypbs

---
+++ MyPBs/MyPBs/MyPBs/ViewModels/PrincipalPageViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MyPBs.ViewModels
+{
+    public class PrincipalPageViewModel
+    {
+        public Command ClickVer { get; set; }
+
+        public PrincipalPageViewModel()
+        {
+            ClickVer = new Command(ExecuteClickVer);
+
+        }
+
+        private void ExecuteClickVer()
+        {
+            
+        }
+    }
+}
